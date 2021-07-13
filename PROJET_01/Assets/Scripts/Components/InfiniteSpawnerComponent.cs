94a3d28127eba5ce0d15e3c40bdb8aab34fb74d4
Vieux <alexlosc@live.fr>
2021-07-13 15:02:03 +0200

    Add resolution anchoring solution + clean data
    + https://gist.github.com/fadookie/256947788c364400abe1
    + Camera Anchoring Component 
    + GameObject Anchoring component 
    + Rename prefab variant that were originals 
    + Sort prefab in correct folders 
    + Add anchoring component on all relevant prefab used in original level 
    + Add uv texture for placeholders 
    + Add texture to limits to see them 
    + Modify project settings for dynamic resolution (frame settings) 
    + Add splash container in original level in order to put our splash somewhere in the level 
    + Clean component in splash prefab

--- PROJET_01/Assets/Scripts/Components/InfiniteSpawnerComponent.cs
+++ PROJET_01/Assets/Scripts/Components/InfiniteSpawnerComponent.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class InfiniteSpawnerComponent : SpawnerBehavior
 {
-
+    public Transform splashContainer; 
 
     public override void SpawnSplash()
     {
         if (spawnTraj.spawnDirections[0] != null)
         {
-            Instantiate(splashPrefab, spawnTraj.spawnDirections[0].target.transform.position, spawnTraj.spawnDirections[0].target.transform.rotation);
+            Instantiate(splashPrefab, spawnTraj.spawnDirections[0].target.transform.position, spawnTraj.spawnDirections[0].target.transform.rotation, splashContainer.transform);
             
         }
     }
-
 }
