108e51a24f1ad4bcfe212cd85794efe703a212f2
Vieux <alexlosc@live.fr>
2021-10-10 12:37:57 +0200

    Fix splash behavior + Add OOB
    + Fix points variable called in event when splash enter the basket 
    + Fix level prefab with OOB colliders preventing splashes to live indefinitely

--- PROJET_01/Assets/Scripts/Components/BasketComponent.cs
+++ PROJET_01/Assets/Scripts/Components/BasketComponent.cs
@@ -6,23 +6,18 @@ public class BasketComponent : MonoBehaviour
 {
     public delegate void Scoring(int points);
     public static event Scoring Score;
+
     private void OnTriggerEnter2D(Collider2D _other)
     {
-
         if (_other.CompareTag("Splash"))
         {
             if (Score != null)
             {
-                Score(_other.GetComponent<SplashBehavior>().splashworthPoints);
+                Score(_other.GetComponent<SplashBehavior>().splash.baseScore);
             }
             Debug.Log("Scored !");
 
-            
             Destroy(_other.gameObject);
-
-            
         }
-
     }
-   
 }
